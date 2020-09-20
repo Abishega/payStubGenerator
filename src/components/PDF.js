d073e0857e3e49911cc44f9bcb4ca36afcd5f6b4
Abishega <abishega@CLLAP-CHN-0624>
2020-09-20 21:04:23 +0530

    Deploy the paystub generator app

---
+++ src/components/PDF.js
@@ -0,0 +1,133 @@
+import React from "react";
+import Pdf from "react-to-pdf";
+import * as ReactBootStrap from "react-bootstrap";
+const ref = React.createRef();
+
+const PDF = (props) => {
+  const {
+    amountPerHour = 0,
+    compAddress1 = "1480 Fall River Road Unit#207",
+    compAddress2 = "Fall River, NS B2T1J1",
+    compAddress3 = "",
+    compName = "Maritime Outlets Inc",
+    cppDeduction = 0,
+    currentTotalAfterDeduction = 0,
+    currentTotalDeduction = 0,
+    eiDeduction = 0,
+    empAddress1 = "Employee ID: 224",
+    empAddress2 = "36 Transom Dr",
+    empAddress3 = "Halifax, NS",
+    empId = "",
+    empName = "Mahima Chachra",
+    itDeduction = 0,
+    payDate = "",
+    payPeriod = "",
+    thisPeriod = 0,
+    workedHours = 0,
+    yearToDate = 0,
+    yearToDateAfterDeduction = 0,
+    yearToDateDeduction = 0,
+  } = props.info;
+  return (
+    <>
+      <div className="PaySlip" ref={ref}>
+        <ReactBootStrap.Table striped bordered hover>
+          <thead>
+            <tr class="noBorder">
+              <th>{compName}</th>
+              <th style={{ "text-align": "right" }}>{empName}</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr class="noBorder">
+              <td colSpan="1"></td>
+              <td style={{ "text-align": "right" }}>{empId}</td>
+            </tr>
+            <tr class="noBorder">
+              <td>{compAddress1}</td>
+              <td style={{ "text-align": "right" }}>{empAddress1}</td>
+            </tr>
+
+            <tr class="noBorder">
+              <td>{compAddress2}</td>
+              <td style={{ "text-align": "right" }}>{empAddress2}</td>
+            </tr>
+            <tr class="noBorder">
+              <td>{compAddress3}</td>
+              <td style={{ "text-align": "right" }}>{empAddress3}</td>
+            </tr>
+
+            <tr style={{ "font-weight": "bold" }} class="noBorder">
+              <td>{payPeriod}</td>
+              <td style={{ "text-align": "right" }}>{payDate}</td>
+            </tr>
+          </tbody>
+        </ReactBootStrap.Table>
+        <ReactBootStrap.Table striped bordered hover>
+          <thead>
+            <tr>
+              <th>Income</th>
+              <th>This Period</th>
+              <th>Year to Date</th>
+              <th>Deductions</th>
+              <th>Current Total</th>
+              <th>Year to Date</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Regular</td>
+              <td>{thisPeriod}</td>
+              <td>{thisPeriod}</td>
+              <td>CPP</td>
+              <td>{cppDeduction}</td>
+              <td>{cppDeduction}</td>
+            </tr>
+            <tr>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td>EI</td>
+              <td>{eiDeduction}</td>
+              <td>{eiDeduction}</td>
+            </tr>
+            <tr>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td>Income Tax</td>
+              <td>{itDeduction}</td>
+              <td>{itDeduction}</td>
+            </tr>
+            <tr style={{ "font-weight": "bold" }}>
+              <td colSpan="3"></td>
+              <td>Total Deductions</td>
+              <td>{currentTotalDeduction}</td>
+              <td>{yearToDateDeduction}</td>
+            </tr>
+            <tr style={{ "font-weight": "bold" }}>
+              <td>Total Gross</td>
+              <td>{thisPeriod}</td>
+              <td>{yearToDate}</td>
+              <td>Total Net</td>
+              <td>{currentTotalAfterDeduction}</td>
+              <td>{yearToDateAfterDeduction}</td>
+            </tr>
+            <tr style={{ "font-weight": "bold" }}>
+              <td colSpan="3"></td>
+              <td>Current Period Net income</td>
+              <td colSpan="1"></td>
+              <td>{yearToDateAfterDeduction}</td>
+            </tr>
+          </tbody>
+        </ReactBootStrap.Table>
+      </div>
+      <div className = "PaySlip"> 
+      <Pdf targetRef={ref} filename="payslip.pdf"  >
+        {({ toPdf }) => <button onClick={toPdf}>Capture as PDF</button>}
+      </Pdf>
+      </div>
+    </>
+  );
+};
+export default PDF;
---
+++ src/components/PaySlip.js
@@ -0,0 +1,313 @@
+import React, { Component } from "react";
+import PDF from "./PDF";
+
+class PaySlip extends Component {
+  state = {
+    compName: "",
+    compAddress1: "",
+    compAddress2: "",
+    compAddress3: "",
+    empName: "",
+    empId: "",
+    empAddress1: "",
+    empAddress2: "",
+    empAddress3: "",
+    payPeriod: "",
+    payDate: "",
+    workedHours: 0,
+    amountPerHour: 0,
+    cppDeduction: 0,
+    eiDeduction: 0,
+    itDeduction: 0,
+    infoSubmitted: false,
+    thisPeriod: 0,
+    yearToDate: 0,
+    currentTotalDeduction: 0,
+    yearToDateDeduction: 0,
+    currentTotalAfterDeduction: 0,
+    yearToDateAfterDeduction: 0,
+  };
+
+  onValChange = (input) => (e) => {
+    this.setState({
+      [input]: e.target.value,
+    });
+  };
+
+  onSubmit = (e) => {
+    if (
+      !this.state.compName ||
+      !this.state.compAddress1 ||
+      !this.state.compAddress2 ||
+      !this.state.empName ||
+      !this.state.empId ||
+      !this.state.empAddress1 ||
+      !this.state.empAddress2 ||
+      !this.state.payPeriod ||
+      !this.state.payDate ||
+      !this.state.amountPerHour ||
+      !this.state.workedHours ||
+      !this.state.cppDeduction ||
+      !this.state.eiDeduction ||
+      !this.state.itDeduction
+    ) {
+      alert("Please Provide Mandatory Information");
+      e.preventDefault();
+    } else {
+      const income = this.state.workedHours * this.state.amountPerHour;
+      this.setState({
+        thisPeriod: income,
+        yearToDate: income,
+      });
+      const additiveDeduction =
+        +this.state.cppDeduction +
+        +this.state.eiDeduction +
+        +this.state.itDeduction;
+      const fixedAdditiveDeduction = additiveDeduction.toFixed(2);
+      this.setState({
+        currentTotalDeduction: fixedAdditiveDeduction,
+        yearToDateDeduction: fixedAdditiveDeduction,
+      });
+      const totalAfterDeduction = income - additiveDeduction;
+      const fixedTotalAfterDeduction = totalAfterDeduction.toFixed(2);
+
+      this.setState({
+        currentTotalAfterDeduction: fixedTotalAfterDeduction,
+        yearToDateAfterDeduction: fixedTotalAfterDeduction,
+      });
+      this.setState({
+        infoSubmitted: true,
+      });
+    }
+  };
+  render() {
+    return (
+      <>
+        {this.state.infoSubmitted === false ? (
+          <div className="container">
+            <div className="jumbotron mt-3">
+              <div className="row">
+                <div className="col-md-12">
+                  <div className="well well-sm">
+                    <form className="from-horizontal" method="post">
+                      <fieldset>
+                        <legend className="text-center header">
+                          Add Details
+                        </legend>
+                        <div className="form-group">
+                          <span className="col-md-1 col-md-offset-2 text-center">
+                            <i className="fa fa-user bigicon"></i>
+                          </span>
+                          <input
+                            onChange={this.onValChange("compName")}
+                            name="title"
+                            type="text"
+                            placeholder="Enter Company Name"
+                            className="form-control"
+                          ></input>
+                        </div>
+                        <div className="form-group">
+                          <span className="col-md-1 col-md-offset-2 text-center">
+                            <i className="fa fa-user bigicon"></i>
+                          </span>
+                          <input
+                            onChange={this.onValChange("compAddress1")}
+                            name="title"
+                            type="text"
+                            placeholder="Enter Company Address (Line 1)"
+                            className="form-control"
+                          ></input>
+                        </div>
+                        <div className="form-group">
+                          <span className="col-md-1 col-md-offset-2 text-center">
+                            <i className="fa fa-user bigicon"></i>
+                          </span>
+                          <input
+                            onChange={this.onValChange("compAddress2")}
+                            name="title"
+                            type="text"
+                            placeholder="Enter Company Address (Line 2)"
+                            className="form-control"
+                          ></input>
+                        </div>
+                        <div className="form-group">
+                          <span className="col-md-1 col-md-offset-2 text-center">
+                            <i className="fa fa-user bigicon"></i>
+                          </span>
+                          <input
+                            onChange={this.onValChange("compAddress3")}
+                            name="title"
+                            type="text"
+                            placeholder="Enter Company Address (Line 3) (optional)"
+                            className="form-control"
+                          ></input>
+                        </div>
+                        <div className="form-group">
+                          <span className="col-md-1 col-md-offset-2 text-center">
+                            <i className="fa fa-user bigicon"></i>
+                          </span>
+                          <input
+                            onChange={this.onValChange("empName")}
+                            name="title"
+                            type="text"
+                            placeholder="Enter Employee Name"
+                            className="form-control"
+                          ></input>
+                        </div>
+                        <div className="form-group">
+                          <span className="col-md-1 col-md-offset-2 text-center">
+                            <i className="fa fa-user bigicon"></i>
+                          </span>
+                          <input
+                            onChange={this.onValChange("empId")}
+                            name="title"
+                            type="text"
+                            placeholder="Enter Employee ID"
+                            className="form-control"
+                          ></input>
+                        </div>
+                        <div className="form-group">
+                          <span className="col-md-1 col-md-offset-2 text-center">
+                            <i className="fa fa-user bigicon"></i>
+                          </span>
+                          <input
+                            onChange={this.onValChange("empAddress1")}
+                            name="title"
+                            type="text"
+                            placeholder="Enter Employee Address(Line 1)"
+                            className="form-control"
+                          ></input>
+                        </div>
+                        <div className="form-group">
+                          <span className="col-md-1 col-md-offset-2 text-center">
+                            <i className="fa fa-user bigicon"></i>
+                          </span>
+                          <input
+                            onChange={this.onValChange("empAddress2")}
+                            name="title"
+                            type="text"
+                            placeholder="Enter Employee Address(Line 2)"
+                            className="form-control"
+                          ></input>
+                        </div>
+                        <div className="form-group">
+                          <span className="col-md-1 col-md-offset-2 text-center">
+                            <i className="fa fa-user bigicon"></i>
+                          </span>
+                          <input
+                            onChange={this.onValChange("empAddress3")}
+                            name="title"
+                            type="text"
+                            placeholder="Enter Employee Address(Line 3) (optional)"
+                            className="form-control"
+                          ></input>
+                        </div>
+                        <div className="form-group">
+                          <span className="col-md-1 col-md-offset-2 text-center">
+                            <i className="fa fa-user bigicon"></i>
+                          </span>
+                          <input
+                            onChange={this.onValChange("payPeriod")}
+                            name="title"
+                            type="text"
+                            placeholder="Enter Pay Period"
+                            className="form-control"
+                          ></input>
+                        </div>
+                        <div className="form-group">
+                          <span className="col-md-1 col-md-offset-2 text-center">
+                            <i className="fa fa-user bigicon"></i>
+                          </span>
+                          <input
+                            onChange={this.onValChange("payDate")}
+                            name="title"
+                            type="text"
+                            placeholder="Enter Pay Date"
+                            className="form-control"
+                          ></input>
+                        </div>
+                        <div className="form-group">
+                          <span className="col-md-1 col-md-offset-2 text-center">
+                            <i className="fa fa-user bigicon"></i>
+                          </span>
+                          <input
+                            onChange={this.onValChange("workedHours")}
+                            name="title"
+                            type="number"
+                            placeholder="Enter Number of Worked Hours"
+                            className="form-control"
+                          ></input>
+                        </div>
+                        <div className="form-group">
+                          <span className="col-md-1 col-md-offset-2 text-center">
+                            <i className="fa fa-user bigicon"></i>
+                          </span>
+                          <input
+                            onChange={this.onValChange("amountPerHour")}
+                            name="title"
+                            type="number"
+                            placeholder="Enter Number of Amount per Hour"
+                            className="form-control"
+                          ></input>
+                        </div>
+                        <div className="form-group">
+                          <span className="col-md-1 col-md-offset-2 text-center">
+                            <i className="fa fa-user bigicon"></i>
+                          </span>
+                          <input
+                            onChange={this.onValChange("cppDeduction")}
+                            name="title"
+                            type="number"
+                            placeholder="Enter CPP"
+                            className="form-control"
+                          ></input>
+                        </div>
+                        <div className="form-group">
+                          <span className="col-md-1 col-md-offset-2 text-center">
+                            <i className="fa fa-user bigicon"></i>
+                          </span>
+                          <input
+                            onChange={this.onValChange("eiDeduction")}
+                            name="title"
+                            type="number"
+                            placeholder="Enter EI"
+                            className="form-control"
+                          ></input>
+                        </div>
+                        <div className="form-group">
+                          <span className="col-md-1 col-md-offset-2 text-center">
+                            <i className="fa fa-user bigicon"></i>
+                          </span>
+                          <input
+                            onChange={this.onValChange("itDeduction")}
+                            name="title"
+                            type="number"
+                            placeholder="Enter Income Tax"
+                            className="form-control"
+                          ></input>
+                        </div>
+                        <div className="form-group">
+                          <button
+                            onClick={this.onSubmit}
+                            type="button"
+                            className="btn btn-primary btn-lg"
+                          >
+                            Generate Pay Structure
+                          </button>
+                        </div>
+                      </fieldset>
+                    </form>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        ) : (
+          <PDF info={this.state} />
+        )}
+      </>
+    );
+  }
+}
+
+export default PaySlip;
